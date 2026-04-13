3bc6c38d07cf72e018a8222a2758a98fc79609f6
NotaWhaIe <tom_yinkes@inbox.ru>
2026-04-13 20:19:53 +0300

    Update to latest Apps Script exec URL

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.SF36_CONFIG = {
   googleSheetsSpreadsheetUrl:
     "https://docs.google.com/spreadsheets/d/1KUxUNeKrTM2DKtfris1Ix5QHo7PG0Q8s2p0L_Cvh3w0/edit?usp=sharing",
   googleSheetsWebAppUrl:
-    "https://script.google.com/macros/s/AKfycbxsItX7jPxMHy0nOF3Y_arBWald7okBcPKoRUihOSre7NELuL-fYc7SmetghzcvXX9t0g/exec",
+    "https://script.google.com/macros/s/AKfycbwfXIyM-NCcodHm21--q87wE3By9zLybcoeYv7pFJQmXUQ1i9IT8QrxYq3DJL4GnHrZhA/exec",
 };
