0e22104e93dbc5533f4b3311e116bb0ba799fde6
NotaWhaIe <tom_yinkes@inbox.ru>
2026-04-13 20:00:06 +0300

    Configure Apps Script web app endpoint

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.SF36_CONFIG = {
   googleSheetsSpreadsheetUrl:
     "https://docs.google.com/spreadsheets/d/1KUxUNeKrTM2DKtfris1Ix5QHo7PG0Q8s2p0L_Cvh3w0/edit?usp=sharing",
-  googleSheetsWebAppUrl: "",
+  googleSheetsWebAppUrl:
+    "https://script.google.com/macros/s/AKfycbxsItX7jPxMHy0nOF3Y_arBWald7okBcPKoRUihOSre7NELuL-fYc7SmetghzcvXX9t0g/exec",
 };
